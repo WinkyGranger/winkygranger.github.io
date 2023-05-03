6f6b1d080055b46a9c1f4c935ec6f66e3c446855
WinkyGranger <winky1230@qq.com>
2023-05-03 17:13:23 +0800

    Site updated: 2023-05-03 17:13:05

--- js/toc.js
+++ js/toc.js
@@ -1,80 +1,92 @@
 (function (window, document) {
-  function register($toc) {
-    const currentInView = new Set();
-    const headingToMenu = new Map();
-    const $menus = Array.from($toc.querySelectorAll('.menu-list > li > a'));
 
-    for (const $menu of $menus) {
-      const elementId = $menu.getAttribute('href').trim().slice(1);
-      const $heading = document.getElementById(elementId);
-      if ($heading) {
-        headingToMenu.set($heading, $menu);
-      }
+    function scrollTo(id) {
+        var element = document.getElementById(id);
+        var headerOffset = -20;
+        var elementPosition = element.offsetTop;
+        var offsetPosition = elementPosition - headerOffset;
+        document.documentElement.scrollTop = offsetPosition;
+        document.body.scrollTop = offsetPosition; // For Safari
     }
 
-    const $headings = Array.from(headingToMenu.keys());
+    function register($toc) {
+      const currentInView = new Set();
+      const headingToMenu = new Map();
+      const $menus = Array.from($toc.querySelectorAll('.menu-list > li > a'));
+  
+      for (const $menu of $menus) {
+        const elementId = $menu.getAttribute('href').trim().slice(1);
+        const $heading = document.getElementById(elementId);
 
-    const callback = (entries) => {
-      for (const entry of entries) {
-        if (entry.isIntersecting) {
-          currentInView.add(entry.target);
-        } else {
-          currentInView.delete(entry.target);
+        if ($heading) {
+          headingToMenu.set($heading, $menu);
         }
       }
-      let $heading;
-      if (currentInView.size) {
-        // heading is the first in-view heading
-        $heading = [...currentInView].sort(($el1, $el2) => $el1.offsetTop - $el2.offsetTop)[0];
-      } else if ($headings.length) {
-        // heading is the closest heading above the viewport top
-        $heading = $headings
-          .filter(($heading) => $heading.offsetTop < window.scrollY)
-          .sort(($el1, $el2) => $el2.offsetTop - $el1.offsetTop)[0];
-      }
-      if ($heading && headingToMenu.has($heading)) {
-        $menus.forEach(($menu) => $menu.classList.remove('is-active'));
-
-        const $menu = headingToMenu.get($heading);
-        $menu.classList.add('is-active');
-        let $menuList = $menu.parentElement.parentElement;
-        while (
-          $menuList.classList.contains('menu-list') &&
-          $menuList.parentElement.tagName.toLowerCase() === 'li'
-        ) {
-          $menuList.parentElement.children[0].classList.add('is-active');
-          $menuList = $menuList.parentElement.parentElement;
-        }
-      }
-    };
-    const observer = new IntersectionObserver(callback, { threshold: 0 });
-
-    for (const $heading of $headings) {
-      observer.observe($heading);
-      // smooth scroll to the heading
-      if (headingToMenu.has($heading)) {
-        const $menu = headingToMenu.get($heading);
-        $menu.setAttribute('data-href', $menu.getAttribute('href'));
-        $menu.setAttribute('href', 'javascript:;');
-        $menu.addEventListener('click', () => {
-          if (typeof $heading.scrollIntoView === 'function') {
-            $heading.scrollIntoView({ behavior: 'smooth' });
-          }
-          const anchor = $menu.getAttribute('data-href');
-          if (history.pushState) {
-            history.pushState(null, null, anchor);
+  
+      const $headings = Array.from(headingToMenu.keys());
+  
+      const callback = (entries) => {
+        for (const entry of entries) {
+          if (entry.isIntersecting) {
+            currentInView.add(entry.target);
           } else {
-            location.hash = anchor;
+            currentInView.delete(entry.target);
           }
-        });
-        $heading.style.scrollMargin = '1em';
+        }
+        let $heading;
+        if (currentInView.size) {
+          // heading is the first in-view heading
+          $heading = [...currentInView].sort(($el1, $el2) => $el1.offsetTop - $el2.offsetTop)[0];
+        } else if ($headings.length) {
+          // heading is the closest heading above the viewport top
+          $heading = $headings
+            .filter(($heading) => $heading.offsetTop < window.scrollY)
+            .sort(($el1, $el2) => $el2.offsetTop - $el1.offsetTop)[0];
+        }
+        if ($heading && headingToMenu.has($heading)) {
+          $menus.forEach(($menu) => $menu.classList.remove('is-active'));
+  
+          const $menu = headingToMenu.get($heading);
+          $menu.classList.add('is-active');
+          let $menuList = $menu.parentElement.parentElement;
+          while (
+            $menuList.classList.contains('menu-list') &&
+            $menuList.parentElement.tagName.toLowerCase() === 'li'
+          ) {
+            $menuList.parentElement.children[0].classList.add('is-active');
+            $menuList = $menuList.parentElement.parentElement;
+          }
+        }
+      };
+      const observer = new IntersectionObserver(callback, { threshold: 0 });
+  
+      for (const $heading of $headings) {
+        observer.observe($heading);
+        // smooth scroll to the heading
+        if (headingToMenu.has($heading)) {
+          const $menu = headingToMenu.get($heading);
+          $menu.setAttribute('data-href', $menu.getAttribute('href'));
+          $menu.setAttribute('href', 'javascript:;');
+          $menu.addEventListener('click', () => {
+            if (typeof $heading.scrollTo === 'function') {
+            //   $heading.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            scrollTo($heading.id);
+            }
+            const anchor = $menu.getAttribute('data-href');
+            if (history.pushState) {
+              history.pushState(null, null, anchor);
+            } else {
+              location.hash = anchor;
+            }
+          });
+          $heading.style.scrollMargin = '1em';
+        }
       }
     }
-  }
-
-  if (typeof window.IntersectionObserver === 'undefined') {
-    return;
-  }
-
-  document.querySelectorAll('#toc').forEach(register);
-})(window, document);
+  
+    if (typeof window.IntersectionObserver === 'undefined') {
+      return;
+    }
+  
+    document.querySelectorAll('#toc').forEach(register);
+  })(window, document);
